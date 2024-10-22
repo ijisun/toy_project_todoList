3f16d6657e78e18baa99fd2d9b06c254d41ffbe4
Jisun Lee <ijisun9292@gmail.com>
2024-10-22 21:08:23 +0900

    fourth commit

--- main.js
+++ main.js
@@ -1,19 +1,47 @@
-// Add 버튼 눌렀을 때 실행할 함수
-function submit(){
-  let add = document.querySelector("#add"); // 할일 입력 창에 접근
-  let list = document.getElementById("todo-area"); // 추가한 값을 추가할 ul 영역에 접근
-  let contents = document.createElement("li"); // li 태그 생성
-  let checkbox = document.createElement("input"); // input 태그 생성
-  let newP = document.createElement("p"); // p 태그 생성
-  let newText = document.createTextNode(add.value); // 입력한 값을 텍스트 노드로 생성
-  
-  if(add.value.length > 0){ // 입력 창이 비어있지 않을 때만 실행하도록 
-    contents.setAttribute("id","todo"); // li 태그에 id="todo" 속성 추가
-    list.appendChild(contents); // ul 태그에 li 태그를 자식 노드로 추가
-    contents.appendChild(checkbox); // li 태그에 input 태그를 자식 노드로 추가
-    contents.appendChild(newP); // li 태그에 p 태그를 자식 노드로 추가
-    newP.appendChild(newText); // p 태그에 text 노드를 자식 노드로 추가
-    checkbox.type = "checkbox" // input 태그의 타입을 checkbox로 설정
-    add.value = ""; // 제출한 뒤 입력 창 비우기
+// 할 일 추가 - 완료
+// 각 할 일에 삭제와 체크 버튼 - 완료
+// 삭제 버튼을 클릭하면 할 일을 리스트에서 삭제
+// 체크 버튼을 클릭하면 밑줄
+// 완료된 할 일은 되돌리기 버튼을 클릭하면 되돌림
+// 탭을 이용해 아이템들을 상태별로 나누어 볼 수 있음
+// 모바일 버전 반응형 웹
+
+let taskInput = document.getElementById("task-input");
+let addBtn = document.getElementById("add-btn");
+let taskList = [];
+let checkBtn = document.getElementById("check-btn");
+let deleteBtn = document.getElementById("delete-btn");
+
+addBtn.addEventListener("click",addTask);
+// checkBtn.addEventListener("click",checked);
+
+function addTask(){
+  let taskContent = taskInput.value;
+  taskList.push(taskContent);
+  render();
+}
+function render(){
+  let resultHTML = "";
+  for(let i=0;i<taskList.length;i++){
+    resultHTML += `<div id="task">
+          <div>${taskList[i]}</div>
+          <div>
+            <button id="check-btn" class="icon"><i class="fa-solid fa-check"></i></button>
+            <button id="delete-btn" class="icon"><i class="fa-solid fa-trash-can"></i></button>
+          </div>
+        </div>`
   }
+  document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+// 따라다니는 언더바
+let underLine = document.getElementById("under-line");
+let taskMenu = document.querySelectorAll(".task-tap");
+taskMenu.forEach((menu) =>
+  menu.addEventListener("click",(e)=>indicator(e))
+);
+function indicator(e){
+  underLine.style.left = e.currentTarget.offsetLeft + "px";
+  underLine.style.width = e.currentTarget.offsetWidth + "px";
+  underLine.style.top = e.currentTarget.offsetTop + e.currentTarget.offsetHeight - 2 + "px";
 }
